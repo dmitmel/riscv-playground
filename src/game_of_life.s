969cd0183c40d454716f9a6f6abce2c1de9a2b5a
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2019-12-23 10:48:18 +0200

    [game_of_life] add double buffering

--- src/game_of_life.s
+++ src/game_of_life.s
@@ -9,14 +9,15 @@
 
 /* u64 */ grid_width:  .dword 20
 /* u64 */ grid_height: .dword 10
-/* u8[] */ grid_data: /* = NULL */ .dword 0
+/* [u8] */ grid_data:      /* = NULL */ .dword 0
+/* [u8] */ grid_next_data: /* = NULL */ .dword 0
 
 .text
 
 
 .global main
 main:
-  # i8 main(i8 argc, char* argv[])
+  # fn main(argc: i32, argv: [char*]) -> i32
   addi sp, sp, -8
   sd ra, 0(sp)
 
@@ -25,14 +26,12 @@ main:
   call time
   call srand
 
-  # grid_data = calloc(grid_width * grid_height, sizeof(u8))
-  ld t0, grid_width
-  ld t1, grid_height
-  mul a0, t0, t1
-  li a1, 1
-  call calloc
-  la t0, grid_data
-  sd a0, 0(t0)
+  # grid_alloc_data_unsafe(&grid_data)
+  la a0, grid_data
+  call grid_alloc_data_unsafe
+  # grid_alloc_data_unsafe(&grid_next_data)
+  la a0, grid_next_data
+  call grid_alloc_data_unsafe
 
   # grid_fill_randomly()
   call grid_fill_randomly
@@ -43,6 +42,9 @@ main:
   # free(grid_data)
   ld a0, grid_data
   call free
+  # free(grid_next_data)
+  ld a0, grid_next_data
+  call free
 
   # return 0
   li a0, 0
@@ -52,8 +54,48 @@ main:
   ret
 
 
+grid_alloc_data_unsafe:
+  # fn grid_alloc_data_unsafe(dest_data: [u8]*)
+  addi sp, sp, -16
+  sd ra, 0(sp)
+
+  sd a0, 8(sp)
+
+  # let data: [u8] = malloc(grid_width * grid_height * sizeof(u8))
+  ld t0, grid_width
+  ld t1, grid_height
+  mul a0, t0, t1
+  call malloc
+
+  # *dest_data = data
+  ld t0, 8(sp)
+  sd a0, 0(t0)
+
+  ld ra, 0(sp)
+  addi sp, sp, 16
+  ret
+
+
+grid_swap:
+  # fn grid_swap()
+  addi sp, sp, -8
+  sd ra, 0(sp)
+
+  # grid_data, grid_next_data = grid_next_data, grid_data
+  ld t0, grid_data
+  ld t1, grid_next_data
+  la t2, grid_next_data
+  sd t0, 0(t2)
+  la t2, grid_data
+  sd t1, 0(t2)
+
+  ld ra, 0(sp)
+  addi sp, sp, 8
+  ret
+
+
 grid_fill_randomly:
-  # void grid_fill_randomly()
+  # fn grid_fill_randomly()
   addi sp, sp, -32
   sd ra,  0(sp)
   sd s1,  8(sp)
@@ -61,26 +103,26 @@ grid_fill_randomly:
   sd s3, 24(sp)
 
   ld s1, grid_data
-  # u64 len = grid_width * grid_height
+  # let len: u64 = grid_width * grid_height
   ld t0, grid_width
   ld t1, grid_height
   mul s2, t0, t1
-  # u64 i = 0
+  # let i: u64 = 0
   li s3, 0
 
   grid_fill_randomly_loop0_start:
     # break if i >= len
     bgeu s3, s2, grid_fill_randomly_loop0_end
 
-    # u8 cell = rand() & 1
+    # let cell: u8 = rand() & 1
     call rand
     andi a0, a0, 1
 
-    # grid[i] = cell
+    # grid_data[i] = cell
     add t0, s1, s3
     sb a0, 0(t0)
 
-    # i++
+    # i += 1
     addi s3, s3, 1
     j grid_fill_randomly_loop0_start
   grid_fill_randomly_loop0_end:
@@ -94,7 +136,7 @@ grid_fill_randomly:
 
 
 grid_print:
-  # void grid_print()
+  # fn grid_print()
   addi sp, sp, -48
   sd ra,  0(sp)
   sd s1,  8(sp)
@@ -103,31 +145,32 @@ grid_print:
   sd s4, 32(sp)
   sd s5, 40(sp)
 
+  # let cell_ptr: u8* = grid_data
   ld s1, grid_data
   ld s2, grid_width
   ld s3, grid_height
-  # u64 x = 0
+  # let x: u64 = 0
   li s4, 0
-  # u64 y = 0
+  # let y: u64 = 0
   li s5, 0
 
   grid_print_loop0_start:
-    # break if y >= height
+    # break if y >= grid_height
     bgeu s5, s3, grid_print_loop0_end
 
     # x = 0
     li s4, 0
 
     grid_print_loop1_start:
-      # break if x >= width
+      # break if x >= grid_width
       bgeu s4, s2, grid_print_loop1_end
 
-      # u8 cell = grid[width * y + x] & 1
-      mul t0, s2, s5
-      add t0, t0, s4
-      add t0, t0, s1
-      lb a0, 0(t0)
+      # let cell: u8 = *cell_ptr & 1
+      lb a0, 0(s1)
       andi a0, a0, 1
+
+      # cell_ptr += 1
+      addi s1, s1, 1
 
       # Here I'm using a little trick to avoid branching: first, I multiply cell
       # value by distance from the character # to space (# is located later in
@@ -140,7 +183,7 @@ grid_print:
       addi a0, a0, ' '
       call putchar
 
-      # x++
+      # x += 1
       addi s4, s4, 1
       j grid_print_loop1_start
     grid_print_loop1_end:
@@ -149,7 +192,7 @@ grid_print:
     li a0, '\n'
     call putchar
 
-    # y++
+    # y += 1
     addi s5, s5, 1
     j grid_print_loop0_start
   grid_print_loop0_end:
