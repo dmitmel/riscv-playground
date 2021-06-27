950dbdc73ffbf0baee1f923bd7a693463d16c9da
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2021-06-27 12:32:06 +0300

    [brainfuck] IMPLEMENT ALL INSTRUCTIONS

--- src/brainfuck.s
+++ src/brainfuck.s
@@ -6,30 +6,23 @@
 # received a copy of the CC0 Public Domain Dedication along with this software.
 # If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
+# Probably the slowest brainfuck interpreter in existance.
+
 .data
 
-/*   *u8 */ program_memory_ptr:   /* = NULL */ .dword 0
-/* usize */ program_head_address: /* = 0    */ .dword 0
+/*   *u8 */ program_memory_ptr: /* = NULL */ .dword 0
+/* usize */ program_head_addr:  /* = 0    */ .dword 0
 
-/* *char */ program_text:                /* = NULL */ .dword 0
-/* usize */ program_len:                 /* = 0    */ .dword 0
-/* usize */ program_current_instruction: /* = 0    */ .dword 0
+/* *char */ program_text:       /* = NULL */ .dword 0
+/* usize */ program_len:        /* = 0    */ .dword 0
+/* usize */ program_curr_char:  /* = 0    */ .dword 0
 
 .section .rodata
 
 /* usize */ PROGRAM_MEMORY_LEN: .dword 30000
 
-str_usage: .string "usage: %s [code]\n"
-
-str_command_nop:        .string "nop"
-str_command_move_right: .string "move right"
-str_command_move_left:  .string "move left"
-str_command_increment:  .string "increment"
-str_command_decrement:  .string "decrement"
-str_command_print:      .string "print"
-str_command_read:       .string "read"
-str_command_loop_begin: .string "loop begin"
-str_command_loop_end:   .string "loop end"
+str_usage:        .string "usage: %s [code]\n"
+str_syntax_error: .string "syntax error\n"
 
 .text
 
@@ -69,24 +62,29 @@ main:
   sd a0, 0(t0)
   mv s2, a0
 
-  # program_memory_ptr = malloc(PROGRAM_MEMORY_LEN)
+  # program_memory_ptr = calloc(PROGRAM_MEMORY_LEN, 1)
   ld a0, PROGRAM_MEMORY_LEN
-  call malloc
+  li a1, 1
+  call calloc
   la t0, program_memory_ptr
   sd a0, 0(t0)
 
   # $s1 and $s2 have already been assigned
-  li s3, 0  # let i: u64 = 0
+  la s3, program_curr_char
   main_for_i_start:
-    # break if i >= len
-    bgeu s3, s2, main_for_i_end
+    # break if program_curr_char >= program_len
+    ld t0, 0(s3)
+    bgeu t0, s2, main_for_i_end
 
-    # process_command(program_text[i])
-    add t0, s1, s3
+    # process_command(program_text[program_curr_char])
+    add t0, s1, t0
     lbu a0, 0(t0)
     call process_command
 
-    addi s3, s3, 1  # i += 1
+    # program_curr_char += 1
+    ld t0, 0(s3)
+    addi t0, t0, 1
+    sd t0, 0(s3)
     j main_for_i_start
   main_for_i_end:
 
@@ -117,26 +115,26 @@ process_command:
   jr t0                                 # execute the jump!
 
   process_command_jumptable_start:
-    .rept 0x2A - 0x00 + 1
+    .rept '+' - 0x00
     j command_nop
     .endr
-    j command_increment   # 0x2B
-    j command_read        # 0x2C
-    j command_decrement   # 0x2D
-    j command_print       # 0x2E
-    .rept 0x3B - 0x2F + 1
+    j command_increment   # 0x2B '+'
+    j command_read        # 0x2C ','
+    j command_decrement   # 0x2D '-'
+    j command_print       # 0x2E '.'
+    .rept '<' - '.' - 1
     j command_nop
     .endr
-    j command_move_left   # 0x3C
+    j command_move_left   # 0x3C '<'
     j command_nop
-    j command_move_right  # 0x3E
-    .rept 0x5A - 0x3F + 1
+    j command_move_right  # 0x3E '>'
+    .rept '[' - '>' - 1
     j command_nop
     .endr
-    j command_loop_begin  # 0x5B
+    j command_loop_begin  # 0x5B '['
     j command_nop
-    j command_loop_end    # 0x5D
-    .rept 0xFF - 0x5E + 1
+    j command_loop_end    # 0x5D ']'
+    .rept 0x100 - ']' - 1
     j command_nop
     .endr
   process_command_jumptable_end:
@@ -146,99 +144,225 @@ process_command:
   ret
 
 command_nop:
-  addi sp, sp, -16
-  sd ra, 0(sp)
+  # fn command_nop()
 
-  la a0, str_command_nop
-  call puts
-
-  ld ra, 0(sp)
-  addi sp, sp, 16
   ret
 
 command_move_right:
-  addi sp, sp, -16
-  sd ra, 0(sp)
+  # fn command_move_right()
 
-  la a0, str_command_move_right
-  call puts
+  la t0, program_head_addr
+  ld t1, 0(t0)
+  ld t2, PROGRAM_MEMORY_LEN
 
-  ld ra, 0(sp)
-  addi sp, sp, 16
+  # goto wraparound if program_head_addr >= PROGRAM_MEMORY_LEN - 1
+  addi t2, t2, -1
+  bgeu t1, t2, command_move_right_wraparound
+
+  # program_head_addr += 1
+  addi t1, t1, 1
+  sd t1, 0(t0)
+
   ret
+
+  command_move_right_wraparound:
+    # program_head_addr = 0
+    li t1, 0
+    sd t1, 0(t0)
+
+    ret
 
 command_move_left:
-  addi sp, sp, -16
-  sd ra, 0(sp)
+  # fn command_move_left()
 
-  la a0, str_command_move_left
-  call puts
+  la t0, program_head_addr
+  ld t1, 0(t0)
 
-  ld ra, 0(sp)
-  addi sp, sp, 16
+  # goto wraparound if program_head_addr <= 0
+  li t2, 0
+  bgeu t2, t1, command_move_left_wraparound
+
+  # program_head_addr -= 1
+  addi t1, t1, -1
+  sd t1, 0(t0)
+
   ret
 
+  command_move_left_wraparound:
+    # program_head_addr = PROGRAM_MEMORY_LEN - 1
+    ld t1, PROGRAM_MEMORY_LEN
+    addi t1, t1, -1
+    sd t1, 0(t0)
+
+    ret
+
 command_increment:
-  addi sp, sp, -16
-  sd ra, 0(sp)
+  # fn command_increment()
 
-  la a0, str_command_increment
-  call puts
+  # let cell_ptr: *u8 = &program_memory_ptr[program_head_addr]
+  ld t0, program_memory_ptr
+  ld t1, program_head_addr
+  add t0, t0, t1
 
-  ld ra, 0(sp)
-  addi sp, sp, 16
+  # *cell_ptr += 1
+  lbu t1, 0(t0)
+  addi t1, t1, 1
+  sb t1, 0(t0)
+
   ret
 
 command_decrement:
-  addi sp, sp, -16
-  sd ra, 0(sp)
+  # fn command_decrement()
 
-  la a0, str_command_decrement
-  call puts
+  # let cell_ptr: *u8 = &program_memory_ptr[program_head_addr]
+  ld t0, program_memory_ptr
+  ld t1, program_head_addr
+  add t0, t0, t1
 
-  ld ra, 0(sp)
-  addi sp, sp, 16
+  # *cell_ptr -= 1
+  lbu t1, 0(t0)
+  addi t1, t1, -1
+  sb t1, 0(t0)
+
   ret
 
 command_print:
+  # fn command_print()
   addi sp, sp, -16
   sd ra, 0(sp)
 
-  la a0, str_command_print
-  call puts
+  # putchar(program_memory_ptr[program_head_addr])
+  ld t0, program_memory_ptr
+  ld t1, program_head_addr
+  add t0, t0, t1
+  lbu a0, 0(t0)
+  call putchar
+
+  # fflush(stdout)
+  ld a0, stdout
+  call fflush
 
   ld ra, 0(sp)
   addi sp, sp, 16
   ret
 
 command_read:
+  # fn command_read()
   addi sp, sp, -16
   sd ra, 0(sp)
 
-  la a0, str_command_read
-  call puts
+  # program_memory_ptr[program_head_addr] = getchar()
+  call getchar
+  bge a0, zero, command_read_no_eof
+  li a0, 0  # on EOF
+  command_read_no_eof:
+
+  ld t0, program_memory_ptr
+  ld t1, program_head_addr
+  add t0, t0, t1
+  sb a0, 0(t0)
 
   ld ra, 0(sp)
   addi sp, sp, 16
   ret
 
 command_loop_begin:
-  addi sp, sp, -16
-  sd ra, 0(sp)
+  # fn command_loop_begin()
 
-  la a0, str_command_loop_begin
-  call puts
+  # return if program_memory_ptr[program_head_addr] != 0
+  ld t0, program_memory_ptr
+  ld t1, program_head_addr
+  add t0, t0, t1
+  lbu t1, 0(t0)
+  bnez t1, command_loop_begin_end
 
-  ld ra, 0(sp)
-  addi sp, sp, 16
-  ret
+  li t0, 1                  # let brackets: i64 = 1
+  ld t1, program_text
+  ld t2, program_curr_char  # let i = program_curr_char
+  ld t5, program_len
+  command_loop_begin_while_brackets:
+    bgeu zero, t0, command_loop_begin_while_brackets_end  # break if brackets <= 0
+
+    addi t2, t2, 1  # i += 1
+    add t3, t1, t2
+    bgeu t2, t5, syntax_error  # syntax_error() if i >= program_len
+    lbu t3, 0(t3)   # let chr = program_text[program_curr_char]
+
+    li t4, '['
+    beq t3, t4, command_loop_begin_inc_brackets  # brackets += 1 if chr == '['
+    li t4, ']'
+    beq t3, t4, command_loop_begin_dec_brackets  # brackets -= 1 if chr == ']'
+    j command_loop_begin_while_brackets
+
+    command_loop_begin_inc_brackets:
+      addi t0, t0, 1
+      j command_loop_begin_while_brackets
+    command_loop_begin_dec_brackets:
+      addi t0, t0, -1
+      j command_loop_begin_while_brackets
+
+  command_loop_begin_while_brackets_end:
+
+  la t1, program_curr_char
+  sd t2, 0(t1)  # program_curr_char = i
+
+  command_loop_begin_end:
+    ret
 
 command_loop_end:
+  # fn command_loop_end()
+
+  # return if program_memory_ptr[program_head_addr] == 0
+  ld t0, program_memory_ptr
+  ld t1, program_head_addr
+  add t0, t0, t1
+  lbu t1, 0(t0)
+  beqz t1, command_loop_end_end
+
+  li t0, 1                  # let brackets: i64 = 1
+  ld t1, program_text
+  ld t2, program_curr_char  # let i = program_curr_char
+  command_loop_end_while_brackets:
+    bgeu zero, t0, command_loop_end_while_brackets_end  # break if brackets <= 0
+
+    bgeu zero, t2, syntax_error  # syntax_error() if i <= 0
+    addi t2, t2, -1  # i -= 1
+    add t3, t1, t2
+    lbu t3, 0(t3)    # let chr = program_text[program_curr_char]
+
+    li t4, '['
+    beq t3, t4, command_loop_end_dec_brackets  # brackets -= 1 if chr == '['
+    li t4, ']'
+    beq t3, t4, command_loop_end_inc_brackets  # brackets += 1 if chr == ']'
+    j command_loop_end_while_brackets
+
+    command_loop_end_inc_brackets:
+      addi t0, t0, 1
+      j command_loop_end_while_brackets
+    command_loop_end_dec_brackets:
+      addi t0, t0, -1
+      j command_loop_end_while_brackets
+
+  command_loop_end_while_brackets_end:
+
+  la t1, program_curr_char
+  sd t2, 0(t1)  # program_curr_char = i
+
+  command_loop_end_end:
+    ret
+
+syntax_error:
+  # fn syntax_error() -> never
   addi sp, sp, -16
   sd ra, 0(sp)
 
-  la a0, str_command_loop_end
-  call puts
+  # printf(str_syntax_error)
+  la a0, str_syntax_error
+  call printf
+
+  # exit(2)
+  li a0, 2
+  call exit
 
   ld ra, 0(sp)
   addi sp, sp, 16
